c959b96f26d7a02da8149c6b1e6207948293ad99
Stefan Jankovic <freelance.stefan@gmail.com>
2018-04-06 16:08:59 +0200

    find-digit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# find-digit
+# Find-Digit
+
+Funtion to find all the integer values between 0 and n.
+	Which contain the number d.
+	
+	Example:
+	Input: n = 20, d = 5
+ 	Output: 5, 15 
+
+~AstoVidatu
--- find-digit.js
+++ find-digit.js
@@ -1,15 +1,28 @@
-var number = 50;
-var digit = 1;
-var A = [];
+// 
+// 	Find the digit
+// 	Funtion to find all the integer values between 0 and n.
+// 	Which contain the number d.
+// 	
+// 	Example:
+// 	Input: n = 20, d = 5
+// 	Output: 5, 15
+// 
+// 	~AstoVidatu
 
+var n = 20; 	//Number
+var d = 5;	//Digit to find
+var A = [];		//Array to store every number from 0 to n
 
-function findDigit(number) {
-	for(var i = 1; i < number+1; i++){
-		A[i] = i;
-		if(((A[i]+'').indexOf(digit) > -1) == true){
+function findd(n) {
+	for(var i = 1; i < n+1; i++){
+
+		A[i] = i;	//storing every int
+
+		//finding if the number contains d
+		if(((A[i]+'').indexOf(d) > -1) == true){
 			console.log(A[i]);
 		}
 	}
 }
 
-findDigit(number);
+findd(n);
